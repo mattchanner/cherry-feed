3df1a4203db7b8c373341285068f9108b2f1d260
Matt Channer <mchanner@idbs.com>
2015-12-22 15:06:47 +0000

    Removed non ascii char from file

--- app/webapp.py
+++ app/webapp.py
@@ -1,6 +1,6 @@
 """
   The MIT License (MIT)
-  Copyright © 2015 Matt Channer (mchanner at gmail dot com)
+  Copyright 2015 Matt Channer (mchanner at gmail dot com)
 
   Permission is hereby granted, free of charge, to any person obtaining a
   copy of this software and associated documentation files (the “Software”),
