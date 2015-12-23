66dc23a93cf5084f6c7b79190c0209b91d9e2403
Matt Channer <mchanner@idbs.com>
2015-12-23 15:05:38 +0000

    Debug statement added to test where CI build is getting to

--- tests/runner.py
+++ tests/runner.py
@@ -2,6 +2,8 @@ import unittest
 import os
 import sys
 
+print "Running tests..."
+
 test_dir = os.path.dirname(__file__)
 os.chdir(test_dir)
 os.chdir("..")
@@ -12,8 +14,6 @@ sys.path.insert(0, root_path)
 sys.path.insert(0, test_dir)
 loader = unittest.TestLoader()
 tests = loader.discover(test_dir)
-
-print tests
 
 runner = unittest.TextTestRunner()
 runner.verbosity = 2
